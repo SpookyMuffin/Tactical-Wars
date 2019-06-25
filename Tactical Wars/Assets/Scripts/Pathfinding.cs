9c381f416489ba7b4dcc283ca688b9e73cb09093
SpookyMuffin <rurerco@gmail.com>
2019-06-26 00:14:05 +0200

    A*  redo

---
+++ Tactical Wars/Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    public class Node
+    {
+
+        Node Parent;
+        Vector2 Pos;
+        int F, G, H;
+        bool Walkable;
+
+        public Node(Vector2 posicion, bool valido)
+        {
+            Parent = null;
+            F = 0;
+            G = 0;
+            H = 0;
+            Pos = posicion;
+            Walkable = valido;
+        }
+    }
+    public class ASTAR
+    {
+        List<Node> Closed;
+        List<Node> Open;
+    }
+}
