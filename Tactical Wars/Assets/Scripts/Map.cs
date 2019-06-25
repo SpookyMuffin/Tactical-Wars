976ecc2612d427b4d4ceb2f981cf2eaf7b1ed7fe
SpookyMuffin <rurerco@gmail.com>
2019-06-25 23:13:07 +0200

    A* Removed

--- Tactical Wars/Assets/Scripts/Map.cs
+++ Tactical Wars/Assets/Scripts/Map.cs
@@ -27,64 +27,17 @@ public class Map : MonoBehaviour
         Grid = GameObject.FindGameObjectsWithTag("GridPanel"); //grid en vector
 
         //Las metemos en la matriz
-        int k = 0; 
-        for (int i = 0; i < x; i++){
-            for (int j = 0; j < y; j++){
-                mTiles[i,j] = tiles[k];
+        int k = 0;
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                mTiles[i, j] = tiles[k];
                 mGrid[i, j] = Grid[k];
                 k++;
             }
         }
-
-        Pathfinding();
     }
-
-    public void Pathfinding()
-    {
-        Vector2 inicio = new Vector2(0, 0);
-        Vector2 destino = new Vector2(2, 0);
-        Stack<aStar.Node> path;
-        List<List<aStar.Node>> pathmap = MakeMapPath();
-        aStar.Astar AEstrella = new aStar.Astar(pathmap);
-        aStar.Node aux;
-
-
-
-        path = AEstrella.FindPath(inicio, destino);
-        int n = path.Count;
-        Debug.Log("Numero de nodos para el camino: " + n);
-        for (int i = 0; i < n; i++)
-        {
-            aux = path.Pop();
-            Debug.Log("x: " + aux.Position.x + " " + "y: " + aux.Position.y);
-            Debug.Log("Padre: "+ aux.Parent.Position.x + "," + aux.Parent.Position.y);
-        }
-
-    }
-
-    List<List<aStar.Node>> MakeMapPath()
-    {
-
-        List<List<aStar.Node>> PathMap = new List<List<aStar.Node>>();
-        Vector2 pos;
-        bool walkable;
-        for (int i = 0; i < x; i++)
-        {
-            List<aStar.Node> temp = new List<aStar.Node>();
-            for (int j = 0; j < y; j++)
-            {
-                pos.x = mTiles[i, j].GetComponent<Tile>().x;
-                pos.y = mTiles[i, j].GetComponent<Tile>().y;
-                walkable = !mTiles[i, j].GetComponent<Tile>().notWalkable;
-                
-                temp.Add(new aStar.Node(pos,walkable));
-            }
-            PathMap.Add(temp);
-        }
-
-        return PathMap;
-    }
-
 
     //Para mostrar las rejillas
     public void MostrarGrid()
--- Tactical Wars/Assets/Scripts/aStar.cs
+++ Tactical Wars/Assets/Scripts/aStar.cs
@@ -1,159 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
-using System.Linq;
-using System.Text;
 
-
-public class aStar : MonoBehaviour
+public class ASTAR : MonoBehaviour
 {
-
-    public class Node
+    // Start is called before the first frame update
+    void Start()
     {
-        // Change this depending on what the desired size is for each element in the grid
-        public static int NODE_SIZE = 1;
-        public Node Parent;
-        public Vector2 Position;
-        public Vector2 Center
-        {
-            get
-            {
-                return new Vector2(Position.x + NODE_SIZE / 2, Position.y + NODE_SIZE / 2);
-            }
-        }
-        public float DistanceToTarget;
-        public float Cost;
-        public float F
-        {
-            get
-            {
-                if (DistanceToTarget != -1 && Cost != -1)
-                    return DistanceToTarget + Cost;
-                else
-                    return -1;
-            }
-        }
-        public bool Walkable;
-
-        public Node(Vector2 pos, bool walkable)
-        {
-            Parent = null;
-            Position = pos;
-            DistanceToTarget = -1;
-            Cost = 1;
-            Walkable = walkable;
-        }
+        
     }
 
-    public class Astar
+    // Update is called once per frame
+    void Update()
     {
-        List<List<Node>> Grid;
-        int GridRows
-        {
-            get
-            {
-                return Grid[0].Count;
-                
-            }
-        }
-        int GridCols
-        {
-            get
-            {
-                return Grid.Count;
-            }
-        }
-
-        public Astar(List<List<Node>> grid)
-        {
-            Grid = grid;
-        }
-
-        public Stack<Node> FindPath(Vector2 Start, Vector2 End)
-        {
-            Node start = new Node(new Vector2((int)(Start.x / Node.NODE_SIZE), (int)(Start.y / Node.NODE_SIZE)), true);
-            Node end = new Node(new Vector2((int)(End.x / Node.NODE_SIZE), (int)(End.y / Node.NODE_SIZE)), true);
-
-            Stack<Node> Path = new Stack<Node>();
-            List<Node> OpenList = new List<Node>();
-            List<Node> ClosedList = new List<Node>();
-            List<Node> adjacencies;
-            Node current = start;
-
-            // add start node to Open List
-            OpenList.Add(start);
-
-            while (OpenList.Count != 0 && !ClosedList.Exists(x => x.Position == end.Position))
-            {
-                current = OpenList[0];
-                OpenList.Remove(current);
-                ClosedList.Add(current);
-                adjacencies = GetAdjacentNodes(current);
-
-
-                foreach (Node n in adjacencies)
-                {
-                    if (!ClosedList.Contains(n) && n.Walkable)
-                    {
-                        if (!OpenList.Contains(n))
-                        {
-                            n.Parent = current;
-                            n.DistanceToTarget = Math.Abs(n.Position.x - end.Position.x) + Math.Abs(n.Position.y - end.Position.y);
-                            n.Cost = 1 + n.Parent.Cost;
-                            OpenList.Add(n);
-                            OpenList = OpenList.OrderBy(node => node.F).ToList<Node>();
-                        }
-                    }
-                }
-            }
-
-            // construct path, if end was not closed return null
-            if (!ClosedList.Exists(x => x.Position == end.Position))
-            {
-                return null;
-            }
-
-            // if all good, return path
-            Node temp = ClosedList[ClosedList.IndexOf(current)];
-            while (temp.Parent != start && temp != null)
-            {
-                Path.Push(temp);
-                temp = temp.Parent;
-            }
-            return Path;
-        }
-
-        private List<Node> GetAdjacentNodes(Node n)
-        {
-            Debug.Log("busco adyacentes para el nodo" + n.Position.x+","+n.Position.y);
-            List<Node> temp = new List<Node>();
-
-            int row = (int)n.Position.y;
-            int col = (int)n.Position.x;
-
-            if (row + 1 < GridRows)
-            {
-                temp.Add(Grid[col][row + 1]);
-                Debug.Log("1");
-            }
-            if (row - 1 >= 0)
-            {
-                temp.Add(Grid[col][row - 1]);
-                Debug.Log("2");
-            }
-            if (col - 1 >= 0)
-            {
-                temp.Add(Grid[col - 1][row]);
-                Debug.Log("3");
-            }
-            if (col + 1 < GridCols)
-            {
-                temp.Add(Grid[col + 1][row]);
-                Debug.Log("4");
-            }
-
-            return temp;
-        }
+        
     }
 }
