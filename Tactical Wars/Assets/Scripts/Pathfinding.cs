c2b96664a4b3d8f2ed22336b287c9805656c6cee
SpookyMuffin <rurerco@gmail.com>
2019-06-26 18:10:52 +0200

    Revert "A*"
    This reverts commit 3d5c42c1a5c851e36df23dce5efcedc6ca8d2919.

--- Tactical Wars/Assets/Scripts/Pathfinding.cs
+++ Tactical Wars/Assets/Scripts/Pathfinding.cs
@@ -1,15 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class Pathfinding : MonoBehaviour
 {
     public class Node
     {
+
         Node Parent;
         Vector2 Pos;
-        public float G, H, F;
+        int F, G, H;
         bool Walkable;
 
         public Node(Vector2 posicion, bool valido)
@@ -21,63 +21,12 @@ public class Pathfinding : MonoBehaviour
             Pos = posicion;
             Walkable = valido;
         }
-        public float GetHValue()
-        {
-            F = G + H;
-            return F;
-        }
     }
     public class ASTAR
     {
-        Node[,] Map;
-        int MapRows;
-        int MapCols;
-       
+        List<Node> Closed;
+        List<Node> Open;
 
-        public void SetMap(GameObject[,] Tiles, int ROWS, int COLS)
-        {
-            Map = new Node[ROWS, COLS];
-            
-            for (int i = 0; i < ROWS; i++)
-            {
-                for (int j = 0; j< COLS; j++)
-                {
-                    Map[i, j] = new Node(new Vector2(Tiles[i, j].GetComponent<Tile>().x,
-                        Tiles[i, j].GetComponent<Tile>().y), !Tiles[i, j].GetComponent<Tile>().notWalkable);
-                }
-            }
-        }
-
-        public ASTAR(GameObject[,] Tiles, int r, int c)
-        {
-            SetMap(Tiles, r, c);
-            MapRows = r;
-            MapCols = c;
-        }
-
-        public Stack<Node> FindPath(Vector2 start, Vector2 end)
-        {
-            Stack<Node> Path = new Stack<Node>();
-
-            List<Node> Closed = new List<Node>();
-            List<Node> Open = new List<Node>();
-
-            Node Start = new Node(start, true);
-            Node End = new Node(end, true);
-
-            Node Q;
-
-            Closed.Add(Start);
-
-            while(Closed.Count != 0)
-            {
-                Closed = Closed.OrderBy(x => x.F).ToList();
-
-
-            }
-
-            return Path;
-        }
 
     }
 }
